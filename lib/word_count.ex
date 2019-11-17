932a6f257c522ddc3e849902a04328399cb1c96b
fernando00-00 <fernandoisrael_gonzalez@ucol.mx>
2019-11-16 19:43:56 -0600

    terminado wordcount

--- lib/word_count.ex
+++ lib/word_count.ex
@@ -7,9 +7,13 @@ defmodule WordCount do
   @spec count(String.t()) :: map
   def count(sentence) do
     sentence
+    |> String.replace(~r/[!@#$%^&*_(),.?":{}|<>]/u," ")
+    |> String.downcase()
     |> String.split()
-    |> Enum.reduce(%{}, fn word, cant ->
-    cant
-    |> Map.put_new(word,1)end)
+    |> Enum.reduce(%{}, fn word, count -> count
+    #no entendi muy bien lo que hace la line ade abajo creo que es como un push de js pero en un indice especifico
+    |> Map.put_new(word,0)
+    #esta la entendi menos
+    |> Map.update(word, 1, &(&1+1)) end)
   end
 end
